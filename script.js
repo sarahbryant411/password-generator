85b9b063298940b7dec3e71c773d33442d3d0bd5
github.com:sarahbryant411/portfolio-html-css.git <sarahbryant411>
2023-11-04 18:53:28 -0400

    added prompts to webpage

--- script.js
+++ script.js
@@ -1,4 +1,92 @@
-// Assignment Code
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -9,6 +97,19 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  var password = "";
 
+  var length = prompt("What is the length of your password between 8 and 128 chars long", 8);
+  var lowercase = confirm("Would you like to use lowecase letters in your password?");
+  var uppercase = confirm("Would you like to use any uppercase letters in your password?");
+  var numerics = confirm("Would you like to use numeric characters in your password?");
+  var special = confirm("Would you like to use any special characters in your password?");
+  
+  // ask the user the questions
+  // generate characters for password and assign them to var password
+
+  return password;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
