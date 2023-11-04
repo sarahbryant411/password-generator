b5211ddcbf96be203f9baafb4a84dbdcbd9afb1b
github.com:sarahbryant411/portfolio-html-css.git <sarahbryant411>
2023-11-04 19:08:38 -0400

    added the allowed characters

--- script.js
+++ script.js
@@ -99,13 +99,22 @@ function writePassword() {
 }
 function generatePassword() {
   var password = "";
+  var allowedChars = []
 
   var length = prompt("What is the length of your password between 8 and 128 chars long", 8);
   var lowercase = confirm("Would you like to use lowecase letters in your password?");
+  if (lowercase) allowedChars = allowedChars.concat(lowerCasedCharacters);
+  console.log(allowedChars)
   var uppercase = confirm("Would you like to use any uppercase letters in your password?");
+  if (uppercase) allowedChars = allowedChars.concat(upperCasedCharacters);
+  console.log(allowedChars)
   var numerics = confirm("Would you like to use numeric characters in your password?");
+  if (numerics) allowedChars = allowedChars.concat(numericCharacters);
+  console.log(allowedChars)
   var special = confirm("Would you like to use any special characters in your password?");
-  
+  if (special) allowedChars = allowedChars.concat(specialCharacters);
+  console.log(allowedChars)
+
   // ask the user the questions
   // generate characters for password and assign them to var password
 
