99b99ef127f1a15e048a0dcb0eb27136b89b725a
github.com:sarahbryant411/portfolio-html-css.git <sarahbryant411>
2023-11-06 13:11:46 -0500

    rearranged some of the code to look more appealing and to look like the starter code

--- script.js
+++ script.js
@@ -1,92 +1,19 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.',
-];
+  '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{',  ']',  '[', '~', '-', '_', '.'];
 
 // Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
-];
+  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
-];// Assignment Code
+  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -102,18 +29,23 @@ function generatePassword() {
   var allowedChars = []
 
   var length = prompt("What is the length of your password between 8 and 128 chars long", 8);
+
   var lowercase = confirm("Would you like to use lowecase letters in your password?");
   if (lowercase) allowedChars = allowedChars.concat(lowerCasedCharacters);
-  console.log(allowedChars)
+  console.log(allowedChars);
+
   var uppercase = confirm("Would you like to use any uppercase letters in your password?");
   if (uppercase) allowedChars = allowedChars.concat(upperCasedCharacters);
-  console.log(allowedChars)
+  console.log(allowedChars);
+
   var numerics = confirm("Would you like to use numeric characters in your password?");
   if (numerics) allowedChars = allowedChars.concat(numericCharacters);
-  console.log(allowedChars)
+  console.log(allowedChars);
+
   var special = confirm("Would you like to use any special characters in your password?");
   if (special) allowedChars = allowedChars.concat(specialCharacters);
-  console.log(allowedChars)
+  console.log(allowedChars);
+
   for (let index = 0; index < length; index++) {
     var random = Math.floor(Math.random() * allowedChars.length)
     var char = allowedChars[random]
